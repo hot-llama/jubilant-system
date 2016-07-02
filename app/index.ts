9f8288307c4c7cd489ae38f282687dfe0511b06e
Josh Milo <josh@joshmilo.com>
2016-07-01 19:47:07 -0700

    adding project files

---
+++ app/index.ts
@@ -0,0 +1,6 @@
+import Point from './mod';
+import './styles/index.css';
+
+let myPoint = new Point(6600,300);
+console.log("point", myPoint);
+myPoint.display();
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var precss = require('precss');
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+  entry: "./app/index.ts",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".js"]
+  },
+  module: {
+    loaders: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      {
+        test: /\.tsx?$/, loader: "ts-loader"
+      },
+      {
+        test: /\.css$/,
+        loaders: ["style", "css", "postcss-loader"]
+      }
+    ],
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {test: /\.js$/, loader: "source-map-loader"}
+    ]
+  },
+  postcss: function() {
+    return [precss, autoprefixer]
+  }
+};
