dc618f63bed4bed644a30a0bd675471067506581
Josh Milo <josh@joshmilo.com>
2016-07-04 17:29:47 -0700

    adding wizard to map

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -6,6 +6,7 @@ class GameState extends Phaser.State {
   preload() {
     this.load.image('background', '../assets/dirt-tile.png');
     this.load.spritesheet('monkey', '../assets/n11.png', 30, 30);
+    this.load.spritesheet('wizard', '../assets/n8.png', 30, 30);
   }
 
 	create() {
--- src/states/GameWorld.js
+++ src/states/GameWorld.js
@@ -1,4 +1,4 @@
-import {width, height} from '../constants';
+import {width, height} from "../constants";
 
 class GameWorld extends Phaser.State {
 
@@ -7,8 +7,31 @@ class GameWorld extends Phaser.State {
     super();
 
     let monkey = null;
+    let wizard = null;
     let cursors = null;
 
+  }
+
+  /**
+   *
+   * @param actor - string
+   * @param postionX - number
+   * @param positionY - number
+   */
+  createActor(actor, postionX, positionY) {
+    let actorName = `${actor}`;
+    let newActor = this.game.add.sprite(postionX, positionY, actorName);
+
+    //Instantiate Actor - Monkey
+    this.physics.arcade.enable(newActor);
+    newActor.body.collideWorldBounds = true;
+    // Animations
+    newActor.animations.add('left', [3, 4, 5], 10, true);
+    newActor.animations.add('right', [6, 7, 8], 10, true);
+    newActor.animations.add('down', [0, 1, 2], 10, true);
+    newActor.animations.add('up', [9, 10, 11], 10, true);
+
+    return newActor;
   }
 
   create() {
@@ -16,6 +39,7 @@ class GameWorld extends Phaser.State {
 
     this.add.tileSprite(0, 0, width, height, 'background');
 
+    this.wizard = this.createActor('wizard', 300, 200);
     //Instantiate Actor - Monkey
     this.monkey = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'monkey');
     this.physics.arcade.enable(this.monkey);
@@ -26,33 +50,43 @@ class GameWorld extends Phaser.State {
     this.monkey.animations.add('down', [0, 1, 2], 10, true);
     this.monkey.animations.add('up', [9, 10, 11], 10, true);
 
-    this.add.text(16, 16, "State Example: init", { font: "16px Arial", fill: "#ffffff" });
+    this.add.text(16, 16, "State Example: init", {font:"16px Arial", fill:"#ffffff"});
+    console.log(this.monkey);
   }
 
   update() {
     this.monkey.body.velocity.x = 0;
     this.monkey.body.velocity.y = 0;
+    this.wizard.body.velocity.x = 0;
+    this.wizard.body.velocity.y = 0;
 
     if (this.cursors.left.isDown) {
-      this.monkey.body.velocity.x = -150;
-      this.monkey.animations.play('left');
+      // this.monkey.body.velocity.x = -150;
+      this.wizard.body.velocity.x = -150;
+      // this.monkey.animations.play('left');
+      this.wizard.animations.play('left');
     }
     else if (this.cursors.right.isDown) {
-      this.monkey.body.velocity.x = 150;
-      this.monkey.animations.play('right');
+      this.wizard.body.velocity.x = 150;
+      // this.monkey.animations.play('right');
+      this.wizard.animations.play('right');
     }
     else if (this.cursors.up.isDown) {
-      this.monkey.body.velocity.y = -150;
-      this.monkey.animations.play('up');
+      this.wizard.body.velocity.y = -150;
+      // this.monkey.animations.play('up');
+      this.wizard.animations.play('up');
 
     }
     else if (this.cursors.down.isDown) {
-      this.monkey.body.velocity.y = 150;
-      this.monkey.animations.play('down');
+      this.wizard.body.velocity.y = 150;
+      // this.monkey.animations.play('down');
+      this.wizard.animations.play('down');
     }
     else {
-      this.monkey.animations.stop();
-      // this.monkey.frame(0);
+      // this.monkey.animations.stop();
+      this.wizard.animations.stop();
+      // this.monkey.frame = 1;
+      this.wizard.frame = 1;
     }
   }
 
