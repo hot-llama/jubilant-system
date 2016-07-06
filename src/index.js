c7703badc73f4c747ad83b5753b83dd001f18fe4
Josh Milo <josh@joshmilo.com>
2016-07-05 19:09:55 -0700

    Changes State Name
    Changes state name to provide better separation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import GameState from 'states/GameState';
+import Load from 'states/Load';
 import GameWorld from 'states/GameWorld';
 import { width, height } from './constants';
 
@@ -6,9 +6,9 @@ class Game extends Phaser.Game {
 	constructor(width, height) {
 		super(width, height, Phaser.AUTO, 'content', null);
 
-		this.state.add('GameState', GameState, false);
+		this.state.add('Load', Load, false);
     this.state.add('GameWorld', GameWorld, false);
-		this.state.start('GameState');
+		this.state.start('Load');
   }
 }
 
